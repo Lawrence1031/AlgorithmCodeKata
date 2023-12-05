eb1a88447d1b8dfc4aa8ebe06aec7398f21c92cf
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2023-12-05 09:58:37 +0900

    Update 체육복.cs

--- 프로그래머스/1/42862. 체육복/체육복.cs
+++ 프로그래머스/1/42862. 체육복/체육복.cs
@@ -13,9 +13,14 @@ public class Solution {
         lostList.Sort();
         reserveList.Sort();
         
-        List<int> result = reserveList.Except(lostList).ToList();
-        List<int> realLost = lostList.Except(reserveList).ToList();
-
+        List<int> result = reserveList.Except(lostList).ToList(); // reserveList에서 lostList를 뺀 것
+        List<int> realLost = lostList.Except(reserveList).ToList(); // lostList에서 result를 뺀 것
+        
+        // 그림으로 나타내면, lost에만 있는 숫자 -> 체육복이 없는 학생의 번호
+        // lost와 reserve에 있는 숫자 -> 체육복이 없어졌지만, 자신의 여벌을 입는 학생의 번호
+        // reserve에만 있는 숫자 -> 체육복이 남아서 다른 학생에게 줄 수 있는 학생의 번호
+        // -> 교집합은 그대로 뺀 뒤에, lost에만 있는 숫자를 0으로하고, reserve에서 주는 것으로 변경
+        
         for (int i = 0; i < realLost.Count; i++)
         {
             cnt[realLost[i] - 1] = 0;
