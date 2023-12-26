0584998cf4b1527b41014e7b0fd66c90d831c95f
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2023-12-26 10:39:42 +0900

    [level unrated] Title: 문자열로 변환, Time: 0.86 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        
+        answer = Convert.ToString(n);
+        return answer;
+    }
+}
