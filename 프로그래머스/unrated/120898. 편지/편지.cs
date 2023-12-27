225b2082efea61ff65257668709291c4955a75e4
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2023-12-27 09:41:46 +0900

    [level unrated] Title: 편지, Time: 0.13 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        int answer = 0;
+        
+        answer = 2 * message.Length;
+        return answer;
+    }
+}
