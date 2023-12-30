a156bbf7dfed1e572e596789e5dae0653a42e7a1
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2023-12-30 19:26:56 +0900

    [level unrated] Title: 문자열을 정수로 변환하기, Time: 0.73 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(string n_str) {
+        int answer = 0;
+        
+        answer = Convert.ToInt32(n_str);
+        return answer;
+    }
+}
