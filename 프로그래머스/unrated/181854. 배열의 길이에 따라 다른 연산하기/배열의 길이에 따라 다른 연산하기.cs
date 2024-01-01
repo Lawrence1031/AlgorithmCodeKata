abf7654a4dfcedde707b49f214d0ab61a5619c73
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-01-01 21:54:26 +0900

    [level unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int n) {
+        int[] answer = new int[arr.Length];
+        
+        if (arr.Length % 2 == 1)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                    answer[i] = arr[i] + n;
+                else
+                    answer[i] = arr[i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 1)
+                    answer[i] = arr[i] + n;
+                else
+                    answer[i] = arr[i];
+            }
+        }
+        
+        return answer;
+    }
+}
