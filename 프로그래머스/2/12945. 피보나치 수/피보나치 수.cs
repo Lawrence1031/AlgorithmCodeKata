d2a451372654cbf0c725fed457090119a3ed2df1
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-01-04 09:42:15 +0900

    [level 2] Title: 피보나치 수, Time: 1.76 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -1,22 +1,16 @@
 public class Solution {
-    public int solution(long n) {
+    public int solution(int n) {
         int answer = 0;
         
-        long result = 0;
-        long curNum = 1;
-        long preNum = 0;
+        int curNum = 1;
+        int preNum = 0;
 
         for (int i = 2; i <= n; i++)
         {
-            result = (preNum + curNum) % 1234567;
+            answer = (preNum + curNum) % 1234567;
             preNum = curNum % 1234567;
-            curNum = result % 1234567;
+            curNum = answer % 1234567;
         }
-        
-        if (result > 1234567)
-            result = result % 1234567;
-        
-        answer = (int)result;
         
         return answer;
     }
