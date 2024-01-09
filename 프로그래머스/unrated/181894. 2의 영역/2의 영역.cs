dcaa921a98014db7cc6ae53dcbd623d22b148fa0
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-01-09 12:23:20 +0900

    Update 2의 영역.cs

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.cs
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.cs
@@ -6,44 +6,24 @@ public class Solution {
     public int[] solution(int[] arr) {
         int[] answer = new int[] {};
         
-        int cnt = 0;
-        int tmp = 0;
+        int firstIdx = Array.IndexOf(arr, 2);
+        int lastIdx = Array.LastIndexOf(arr, 2);
         
         List<int> arrList = new List<int> ();
         
         // 처음 2가 나오는 지점과 마지막으로 2가 나오는 지점을 알면 됨.
         // 2가 1개 나오면 [2]를 return, 2개 0개 나오면 [-1]을 return
         
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] == 2)
-                cnt++;
-        }
-        
-        if (cnt == 0)
+        if (firstIdx == -1)
         {
             arrList.Add(-1);
         }
-        else if (cnt == 1)
-        {
-            arrList.Add(2);
-        }
         else
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = firstIdx; i <= lastIdx; i++)
             {
-                if (arr[i] == 2)
-                {
-                    tmp++;
-                }
-                
-                if (tmp > 0 && cnt > tmp)
-                {
-                    arrList.Add(arr[i]);
-                }
+                arrList.Add(arr[i]);
             }
-            
-            arrList.Add(2);
         }
         
         answer = arrList.ToArray();
