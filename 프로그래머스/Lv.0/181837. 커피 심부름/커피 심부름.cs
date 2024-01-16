6a03f43773e475c5957a864b84b98bbf3d17b5b3
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-01-16 14:03:11 +0900

    [level Lv.0] Title: 커피 심부름, Time: 0.28 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181837. 커피 심부름/커피 심부름.cs
+++ 프로그래머스/Lv.0/181837. 커피 심부름/커피 심부름.cs
@@ -4,14 +4,10 @@ public class Solution {
     public int solution(string[] order) {
         int answer = 0;
         
-        for (int i = 0; i < order.Length; i++)
+        foreach (string choice in order)
         {
-            if (order[i].Contains('n'))
-                answer += 4500;
-            else
-                answer += 5000;
-        }
-        
+            answer += choice.Contains('n') ? 4500 : 5000;
+        }        
         
         return answer;
     }
