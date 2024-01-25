cb3f83787dce233223f00728d101812d1f0c5b9d
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-01-25 21:29:21 +0900

    Update 직사각형 넓이 구하기.cs

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cs
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cs
@@ -3,32 +3,20 @@ using System;
 public class Solution {
     public int solution(int[,] dots) {
         int answer = 0;
-        int width = 0;
-        int height = 0;
+        int width = Math.Abs(dots[0, 0] - dots[1, 0]);
+        int height = Math.Abs(dots[0, 1] - dots[1, 1]);
 
-        if (dots[0, 0] - dots[1, 0] != 0)
+        if (width == 0)
         {
-            width = dots[0, 0] - dots[1, 0];
-        }
-        else if (dots[0, 0] - dots[2, 0] != 0)
-        {
-            width = dots[0, 0] - dots[2, 0];
+            width = Math.Abs(dots[0, 0] - dots[2, 0]);
         }
 
-        if (dots[0, 1] - dots[1, 1] != 0)
+        if (height == 0)
         {
-            height = dots[0, 1] - dots[1, 1];
-        }
-        else if (dots[0, 1] - dots[2, 1] != 0)
-        {
-            height = dots[0, 1] - dots[2, 1];
+            height = Math.Abs(dots[0, 1] - dots[2, 1]);
         }
 
         answer = width * height;
-        if (answer < 0)
-        {
-            answer *= -1;
-        }
         
         return answer;
     }
