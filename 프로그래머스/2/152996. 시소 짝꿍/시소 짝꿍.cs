9b2591ea03a74db3cda25101c20c4e72b7a6e194
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-02-08 11:05:25 +0900

    [level 2] Title: 시소 짝꿍, Time: 1.81 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        int cnt = 0;
+        int tmpNum = 0;
+
+        Array.Sort(weights);
+        
+        int[] torque = new int[weights[weights.Length - 1] * 4 + 1];
+
+        foreach (int weight in weights)
+        {
+            torque[weight * 2]++;
+            torque[weight * 3]++;
+            torque[weight * 4]++;
+
+            if (tmpNum == weight)
+            {
+                cnt++;
+                answer -= cnt * 2;
+            }
+            else
+            {
+                tmpNum = weight;
+                cnt = 0;
+            }
+        }
+
+        foreach (int num in torque)
+        {
+            if (num > 1)
+            {
+                answer += CountTorque(num);
+            }
+        }
+        
+        return answer;
+    }
+    private static long CountTorque(int num)
+    {
+        return (long)num * (num - 1) / 2;
+    }
+}
