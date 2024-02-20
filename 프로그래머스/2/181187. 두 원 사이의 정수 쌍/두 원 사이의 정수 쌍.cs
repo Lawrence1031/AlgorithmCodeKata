2a471594154851ab74a37bd40c0e6190df90423e
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-02-20 09:58:02 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 17.59 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Solution {
+    public long solution(int r1, int r2) {
+        
+        long answer = r2 - r1 + 1;
+        long r1Sqaure = (long)Math.Pow(r1, 2);
+        long r2Sqaure = (long)Math.Pow(r2, 2);
+
+        for (int x = 1; x < r2; x++)
+        {
+            long xSquare = (long)Math.Pow(x, 2);
+            int yMax = (int)Math.Sqrt(r2Sqaure - xSquare);
+
+            if (x < r1)
+            {
+                int yMin = (int)Math.Ceiling(Math.Sqrt(r1Sqaure - xSquare));
+                answer += yMax - yMin + 1;
+            }
+            else
+            {
+                answer += yMax;
+            }
+        }
+
+        answer *= 4;
+        
+        return answer;
+    }
+}
