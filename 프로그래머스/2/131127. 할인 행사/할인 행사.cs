6a87d692dea5d79c768def323213c8df9cf9b833
Lawrence1031 <144416099+Lawrence1031@users.noreply.github.com>
2024-03-11 22:17:06 +0900

    Update 할인 행사.cs

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cs
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cs
@@ -5,10 +5,10 @@ public class Solution {
     public int solution(string[] want, int[] number, string[] discount) {
         int answer = 0;
         
-        Dictionary<string, int> discountMap = new Dictionary<string, int>();
-
         for (int i = 0; i <= discount.Length - 10; i++)
         {
+            Dictionary<string, int> discountMap = new Dictionary<string, int>();
+
             for (int j = 0; j < want.Length; j++)
             {
                 discountMap.Add(want[j], number[j]);
@@ -22,21 +22,20 @@ public class Solution {
                 }
             }
 
-            int remainItems = 0;
-            foreach (int dictValue in discountMap.Values)
+            bool allDiscounted = true;
+            foreach (var item in discountMap)
             {
-                if (dictValue > remainItems)
+                if (item.Value > 0)
                 {
-                    remainItems = dictValue;
+                    allDiscounted = false;
+                    break;
                 }
             }
 
-            if (remainItems == 0)
+            if (allDiscounted)
             {
                 answer++;
             }
-
-            discountMap.Clear();
         }
         
         return answer;
