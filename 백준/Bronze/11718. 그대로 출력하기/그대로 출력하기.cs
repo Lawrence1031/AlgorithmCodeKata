4e36ed344803e94b5616166ac998cb1831c0415b
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-05 02:16:32 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 64 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,15 @@
+namespace _11718
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+
+            while ((s = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
