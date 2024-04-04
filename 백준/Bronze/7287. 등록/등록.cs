d0cebef4de331aac46a7e78e7bd6fe207b172d8e
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-04 20:13:15 +0900

    [Bronze V] Title: 등록, Time: 48 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,11 @@
+namespace _7287
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10");
+            Console.WriteLine("lawrence1031h");
+        }
+    }
+}
