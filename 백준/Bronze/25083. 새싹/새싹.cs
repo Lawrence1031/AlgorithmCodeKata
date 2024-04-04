67e5ec12325bc15577bfcdf5bfd87c6f6f98ab42
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-04 20:51:26 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,15 @@
+namespace _25083
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
