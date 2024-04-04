d0b71f6ce00c42c5862b83d8acf32dc446d04380
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-04 20:54:56 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,18 @@
+namespace _10869
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split(" ");
+            int a = Convert.ToInt32(ss[0]);
+            int b = Convert.ToInt32(ss[1]);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
