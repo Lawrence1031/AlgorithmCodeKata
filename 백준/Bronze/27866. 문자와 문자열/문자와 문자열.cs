931487fbb49d02f60f138f0ece2d82d085b8f56d
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-05 01:38:23 +0900

    [Bronze V] Title: 문자와 문자열, Time: 60 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,13 @@
+namespace _27866
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(s[i - 1]);
+        }
+    }
+}
