88630b8b3fec5563b51ed08fc0ff043508a7df2e
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-04 20:01:27 +0900

    [Bronze V] Title: 오늘 날짜, Time: 64 ms, Memory: 5528 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,12 @@
+namespace _10699
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Today = DateTime.Today.ToString("yyyy-MM-dd");
+
+            Console.WriteLine(Today);
+        }
+    }
+}
