aebec0a7e19f1f2cf8abb47553a74085542224ed
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-05 01:11:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,12 @@
+namespace _2743
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int i = s.Length;
+            Console.WriteLine(i);
+        }
+    }
+}
