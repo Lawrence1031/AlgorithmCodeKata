dabdd09f9d0680711c4d091bfe8f88ac77d95c3a
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-12 14:24:26 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 404 ms, Memory: 25964 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cs
@@ -0,0 +1,31 @@
+using System.Text;
+
+namespace _11650
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<(int, int)> list = new List<(int, int)> ();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                list.Add((int.Parse(s[0]), int.Parse(s[1])));
+            }
+
+            var sortedList = list.OrderBy(set => set.Item2)
+                                 .ThenBy(set => set.Item1)
+                                 .ToList();
+
+            foreach (var item in sortedList)
+            {
+                sb.AppendLine($"{item.Item1} {item.Item2}");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
