df8917e0b4b73207378ea949e66887fc1838b617
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-14 22:17:51 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 68 ms, Memory: 5524 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
@@ -0,0 +1,27 @@
+namespace _11727
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 1)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 3;
+
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 2]) % 10007;
+            }
+
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
