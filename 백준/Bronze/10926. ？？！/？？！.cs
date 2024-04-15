7328c33bf1110f10c76f6441b3668b1f0eff115a
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-15 20:53:34 +0900

    [Bronze V] Title: ??!, Time: 68 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,12 @@
+namespace _10926
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input + "??!");
+        }
+    }
+}
