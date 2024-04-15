a0363dd9cbf84956d3572fea8f17c84035af8950
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-16 00:57:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 76 ms, Memory: 6040 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cs
@@ -0,0 +1,36 @@
+namespace _10811
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int N = int.Parse(inputs[0]);
+            int M = int.Parse(inputs[1]);
+            int[] arr = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int a = int.Parse(inputs[0]) - 1;
+                int b = int.Parse(inputs[1]) - 1;
+
+                while (a < b)
+                {
+                    int tmp = arr[a];
+                    arr[a] = arr[b];
+                    arr[b] = tmp;
+                    a++;
+                    b--;
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
