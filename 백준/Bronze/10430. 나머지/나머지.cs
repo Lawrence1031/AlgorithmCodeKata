394ee8ba5d07c9125e28048bac31f52653dbb217
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-15 21:02:31 +0900

    [Bronze V] Title: 나머지, Time: 60 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cs
@@ -0,0 +1,17 @@
+namespace _10430
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int A = int.Parse(inputs[0]);
+            int B = int.Parse(inputs[1]);
+            int C = int.Parse(inputs[2]);
+            Console.WriteLine((A + B) % C);
+            Console.WriteLine(((A % C) + (B % C)) % C);
+            Console.WriteLine((A * B) % C);
+            Console.WriteLine(((A % C) * (B % C) % C));
+        }
+    }
+}
