3621088d020385565cd08401226f7c562197e74b
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-15 20:55:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,12 @@
+namespace _18108
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(input - 543);
+        }
+    }
+}
