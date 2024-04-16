3643e314ed0daa00137448a24cc571cd08d67632
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-17 08:14:07 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,12 @@
+namespace _15894
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long answer = 4 * n;
+            Console.WriteLine(answer);
+        }
+    }
+}
