a543e05bd37a99fec822790ae562f35cc39524dd
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-17 06:40:02 +0900

    [Bronze V] Title: 직사각형, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,13 @@
+namespace _27323
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(A * B);
+        }
+    }
+}
