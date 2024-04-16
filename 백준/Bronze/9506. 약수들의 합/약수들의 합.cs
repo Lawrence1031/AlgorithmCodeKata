5270fd9b60d2a71d64a5df359476f312be801c14
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-17 06:22:50 +0900

    [Bronze I] Title: 약수들의 합, Time: 72 ms, Memory: 6504 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cs
@@ -0,0 +1,46 @@
+using System.Text;
+
+namespace _9506
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = int.Parse(Console.ReadLine());
+                StringBuilder sb = new StringBuilder();
+
+                if (n == -1)
+                {
+                    break;
+                }
+
+                int[] arr = new int[n];
+                int cnt = 0;
+                sb.Append(n + " = ");
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    arr[i] = n % (i + 1);
+                    if (arr[i] == 0)
+                    {
+                        cnt += i + 1;
+                        sb.Append((i + 1) + " + ");
+                    }
+                }
+
+                if (cnt == n)
+                {
+                    sb.Remove(sb.Length - 3, 2);
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    Console.WriteLine(n + " is NOT perfect.");
+                }
+                sb.Clear();
+            }
+        }
+    }
+}
