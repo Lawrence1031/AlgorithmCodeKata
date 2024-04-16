03d7938edb553b1c36834ea401844a7f9bc4902a
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-17 08:20:30 +0900

    [Bronze III] Title: 대지, Time: 88 ms, Memory: 13352 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.cs
+++ 백준/Bronze/9063. 대지/대지.cs
@@ -5,7 +5,6 @@ namespace _9063
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
-            int[,] arr = new int[2, N];
             int minX = 10001;
             int minY = 10001;
             int maxX = -10001;
@@ -16,9 +15,6 @@ namespace _9063
                 string[] inputs = Console.ReadLine().Split(' ');
                 int x = int.Parse(inputs[0]);
                 int y = int.Parse(inputs[1]);
-
-                arr[0, i] = x;
-                arr[1, i] = y;
 
                 minX = Math.Min(minX, x);
                 minY = Math.Min(minY, y);
