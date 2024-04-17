b03296e4a7b1e47c024f544ffe7409ef06a2cfbd
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-17 20:34:37 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 56 ms, Memory: 5416 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cs
@@ -0,0 +1,11 @@
+namespace _24262
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(0);
+        }
+    }
+}
