f6c58b0ab1c3dbd68fbd6b63d372abe8535323f9
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-19 06:06:33 +0900

    [Silver V] Title: 창문 닫기, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cs
@@ -0,0 +1,13 @@
+namespace _13909
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int openCnt = (int)Math.Sqrt(n);
+
+            Console.WriteLine(openCnt);
+        }
+    }
+}
