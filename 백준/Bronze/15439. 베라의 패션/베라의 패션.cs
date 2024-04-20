bf2b0c61757166c0de192efaa3b1e16496065f62
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-21 00:17:05 +0900

    [Bronze IV] Title: 베라의 패션, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cs
@@ -0,0 +1,12 @@
+namespace _15439
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(N * (N - 1));
+        }
+    }
+}
