b06c35473828d460c9104980ebee0875f5904cf2
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-21 00:20:09 +0900

    [Bronze IV] Title: 녹색거탑, Time: 80 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cs
@@ -0,0 +1,12 @@
+namespace _24723
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Pow(2, N));
+        }
+    }
+}
