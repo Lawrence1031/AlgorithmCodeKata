3820a6352627fef2d370b16bf98aea48afc6ddaf
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-04-27 03:00:01 +0900

    [Bronze V] Title: A+B - 2, Time: 64 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,12 @@
+namespace _2558
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine(A + B);
+        }
+    }
+}
