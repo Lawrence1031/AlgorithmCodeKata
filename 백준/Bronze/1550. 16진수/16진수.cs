d72ab556ffbda3addcffe1719f2fea1a4eadd347
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-05-08 11:01:43 +0900

    [Bronze II] Title: 16진수, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cs
@@ -0,0 +1,14 @@
+namespace _1550
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int answer = Convert.ToInt32(input, 16);
+
+            Console.WriteLine(answer);
+        }
+    }
+}
