c7fa0d649669541566539c5bd4c9a743d5762242
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-05-28 12:42:42 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 60 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cs
@@ -0,0 +1,15 @@
+namespace _3765
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
