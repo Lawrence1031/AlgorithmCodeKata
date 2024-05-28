ce70d8a43910af3cb3d094c8ad1958863f90ffe0
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-05-28 12:34:41 +0900

    [Bronze IV] Title: Next in line, Time: 68 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cs
@@ -0,0 +1,13 @@
+namespace _6749
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int youngest = int.Parse(Console.ReadLine());
+            int middle = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(2 * middle - youngest);
+        }
+    }
+}
