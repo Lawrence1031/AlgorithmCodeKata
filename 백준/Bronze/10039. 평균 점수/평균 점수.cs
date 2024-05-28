2c0d43e33f4d3855f5fbef5cce410b722f50a601
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-05-28 12:29:39 +0900

    [Bronze IV] Title: 평균 점수, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cs
@@ -0,0 +1,20 @@
+namespace _10039
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int score = int.Parse(Console.ReadLine());
+
+                if (score <= 40) score = 40;
+                sum += score;
+            }
+
+            Console.WriteLine(sum / 5);
+        }
+    }
+}
