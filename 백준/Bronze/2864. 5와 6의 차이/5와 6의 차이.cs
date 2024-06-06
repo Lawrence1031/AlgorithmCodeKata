06f0ce6b8d0c3f34aa8a50f52af223333b35f92d
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-06-06 18:17:37 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 64 ms, Memory: 5552 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cs
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cs
@@ -7,57 +7,18 @@ namespace _2864
         static void Main(string[] args)
         {
             string[] inputs = Console.ReadLine().Split(' ');
-            string AStr = inputs[0];
-            string BStr = inputs[1];
-            StringBuilder AsbMin = new StringBuilder();
-            StringBuilder AsbMax = new StringBuilder();
-            StringBuilder BsbMin = new StringBuilder();
-            StringBuilder BsbMax = new StringBuilder();
+            string A = inputs[0];
+            string B = inputs[1];
 
-            for (int i = 0; i < AStr.Length; i++)
-            {
-                if (AStr[i] == '5')
-                {
-                    AsbMin.Append('5');
-                    AsbMax.Append('6');
-                }
-                else if (AStr[i] == '6')
-                {
-                    AsbMin.Append('5');
-                    AsbMax.Append('6');
-                }
-                else
-                {
-                    AsbMin.Append(AStr[i]);
-                    AsbMax.Append(AStr[i]);
-                }
-            }
+            string minA = A.Replace('6', '5');
+            string minB = B.Replace('6', '5');
+            int min = int.Parse(minA) + int.Parse(minB);
 
-            for (int i = 0; i < BStr.Length; i++)
-            {
-                if (BStr[i] == '5')
-                {
-                    BsbMin.Append('5');
-                    BsbMax.Append('6');
-                }
-                else if (BStr[i] == '6')
-                {
-                    BsbMin.Append('5');
-                    BsbMax.Append('6');
-                }
-                else
-                {
-                    BsbMin.Append(BStr[i]);
-                    BsbMax.Append(BStr[i]);
-                }
-            }
+            string maxA = A.Replace('5', '6');
+            string maxB = B.Replace('5', '6');
+            int max = int.Parse(maxA) + int.Parse(maxB);
 
-            int AMax = int.Parse(AsbMax.ToString());
-            int AMin = int.Parse(AsbMin.ToString());
-            int BMax = int.Parse(BsbMax.ToString());
-            int BMin = int.Parse(BsbMin.ToString());
-
-            Console.WriteLine($"{AMin + BMin} {AMax + BMax}");
+            Console.WriteLine($"{min} {max}");
         }
     }
 }
