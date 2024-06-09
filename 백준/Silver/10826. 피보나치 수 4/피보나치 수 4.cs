1bd1a78b9fc4ab898fa4269a0a8852023d4d4d14
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-06-09 19:53:22 +0900

    [Silver V] Title: 피보나치 수 4, Time: 92 ms, Memory: 15636 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cs
@@ -0,0 +1,30 @@
+using System.Numerics;
+
+namespace _2748
+{
+    internal class Program
+    {
+        static Dictionary<int, BigInteger> Fibo = new Dictionary<int, BigInteger>();
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(fibonacci(n).ToString());
+        }
+
+        static BigInteger fibonacci(int n)
+        {
+            Fibo[0] = 0;
+            Fibo[1] = 1;
+            Fibo[2] = 1;
+            int cnt = 3;
+
+            while (!Fibo.ContainsKey(n))
+            {
+                Fibo[cnt] = Fibo[cnt - 1] + Fibo[cnt - 2];
+                cnt++;
+            }
+
+            return Fibo[n];
+        }
+    }
+}
