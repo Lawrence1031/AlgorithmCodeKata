6cc1220e67ee0b560f32d1359d3db5b06a5f7f13
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-06-20 20:41:27 +0900

    [Silver V] Title: 돌 게임 2, Time: 92 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cs
@@ -0,0 +1,11 @@
+namespace _9656
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N % 2 != 0 ? "CY" : "SK");
+        }
+    }
+}
