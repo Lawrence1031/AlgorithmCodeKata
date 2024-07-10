215a5adaa5c3c22c6283e3cff9b6baa8ae5c4a85
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-07-11 04:08:12 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cs
@@ -0,0 +1,13 @@
+namespace _16486
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(2 * A + 2 * 3.141592 * B);
+        }
+    }
+}
