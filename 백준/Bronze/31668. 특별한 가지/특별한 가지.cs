2f3281c33199ae8b7db152ec3b4dee8f41acbe71
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-08-03 02:35:41 +0900

    [Bronze IV] Title: 특별한 가지, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cs
@@ -0,0 +1,14 @@
+namespace _31668
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int K = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(M * K / N);
+        }
+    }
+}
