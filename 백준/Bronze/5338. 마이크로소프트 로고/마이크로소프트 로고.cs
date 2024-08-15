14e74ea7e13cd94407c3bf10b7d0b9afaa2bf955
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-08-15 09:27:28 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 44 ms, Memory: 5348 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,19 @@
+using System.Text;
+
+namespace _5338
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("       _.-;;-._");
+            sb.AppendLine("'-..-'|   ||   |");
+            sb.AppendLine("'-..-'|_.-;;-._|");
+            sb.AppendLine("'-..-'|   ||   |");
+            sb.AppendLine("'-..-'|_.-''-._|");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
