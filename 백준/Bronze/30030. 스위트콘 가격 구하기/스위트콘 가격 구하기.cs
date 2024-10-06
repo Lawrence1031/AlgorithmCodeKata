ac9a5b617a031d8652ec1e08ccc501db243cecbb
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-10-06 21:27:11 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cs
@@ -0,0 +1,12 @@
+namespace _30030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int B = int.Parse(Console.ReadLine());
+            double A = Math.Round(B / 1.1);
+            Console.WriteLine(A);
+        }
+    }
+}
