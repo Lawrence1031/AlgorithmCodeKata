be9c7858850931c44fb2674d63494b17a985f81b
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-10-08 03:56:03 +0900

    [Silver IV] Title: 그래서 대회 이름 뭐로 하죠, Time: 68 ms, Memory: 8012 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.cs
+++ 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic;
 using System.Text;
 
 namespace _27466
@@ -12,53 +13,42 @@ namespace _27466
             string S = Console.ReadLine();
 
             HashSet<char> consonants = new HashSet<char>("BCDFGHJKLMNPQRSTVWXYZ");
-            List<int> AIdx = new List<int>();
-
-            for (int i = 0; i < N; i++)
-            {
-                if (S[i] == 'A')
-                {
-                    AIdx.Add(i);
-                }
-            }
-            
-            if (AIdx.Count < 2)
-            {
-                Console.WriteLine("NO");
-                return;
-            }
 
             StringBuilder sb = new StringBuilder();
 
-            for (int i = 0; i < AIdx[AIdx.Count - 2]; i++)
+            for (int i = N - 1; i >= 0; i--)
             {
-                sb.Append(S[i]);
-
-                if (sb.Length == M - 3)
-                {
-                    break;
-                }
-            }
-
-            sb.Append("AA");
-
-            for (int i = AIdx[AIdx.Count - 1]; i < N; i++)
-            {
-                if (consonants.Contains(S[i]))
+                if (sb.Length == 0 && (consonants.Contains(S[i])))
                 {
                     sb.Append(S[i]);
+                }
+                else if ((sb.Length == 1 || sb.Length == 2) && S[i] == 'A')
+                {
+                    sb.Append(S[i]);
+                }
+                else if (sb.Length > 2)
+                {
+                    sb.Append(S[i]);
+                }
+
+                if (sb.Length == M)
+                {
                     break;
                 }
             }
 
-            if (sb.Length == M)
+            if (sb.Length != M)
             {
-                Console.WriteLine("YES");
-                Console.WriteLine(sb.ToString());
+                Console.WriteLine("NO");
             }
             else
             {
-                Console.WriteLine("NO");
+                Console.WriteLine("YES");
+                string answer = sb.ToString();
+                char[] charArr = answer.ToCharArray();
+                Array.Reverse(charArr);
+                answer = new string(charArr);
+                Console.WriteLine(answer);
             }
         }
     }
