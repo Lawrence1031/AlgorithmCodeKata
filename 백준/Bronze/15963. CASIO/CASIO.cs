e0f88b453b4d9335cd970ba87b910c7512e2fd7e
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-10-10 04:21:55 +0900

    [Bronze V] Title: CASIO, Time: 44 ms, Memory: 5380 KB -BaekjoonHub

--- 백준/Bronze/15963. CASIO/CASIO.cs
+++ 백준/Bronze/15963. CASIO/CASIO.cs
@@ -5,10 +5,8 @@ namespace _15963
         static void Main(string[] args)
         {
             string[] inputs = Console.ReadLine().Split(' ');
-            long N = long.Parse(inputs[0]);
-            long M = long.Parse(inputs[1]);
 
-            Console.WriteLine(N == M ? "1" : "0");
+            Console.WriteLine(inputs[0] == inputs[1] ? "1" : "0");
         }
     }
 }
