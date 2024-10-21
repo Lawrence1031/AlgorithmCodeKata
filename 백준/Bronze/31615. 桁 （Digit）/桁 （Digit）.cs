8ce8e01c4f42abdba5351a7d50e12f7804be6f04
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-10-22 08:21:08 +0900

    [Bronze IV] Title: 桁 (Digit), Time: 48 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/31615. 桁 （Digit）/桁 （Digit）.cs
@@ -0,0 +1,15 @@
+namespace _31615
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int C = A + B;
+
+            Console.WriteLine(C.ToString().Length);
+        }
+    }
+}
