989b5a3949421a0f046a7256445d88fac571df63
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-10-24 14:11:32 +0900

    [Silver V] Title: 정보갓 영훈이, Time: 68 ms, Memory: 7244 KB -BaekjoonHub

--- 백준/Silver/14843. 정보갓 영훈이/정보갓 영훈이.cs
+++ 백준/Silver/14843. 정보갓 영훈이/정보갓 영훈이.cs
@@ -1,47 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Program
+namespace _14843
 {
-    static void Main()
+    internal class Program
     {
-        int N = int.Parse(Console.ReadLine());
-        double totalScore = 0;
-
-        for (int i = 0; i < N; i++)
+        static void Main(string[] args)
         {
-            string[] input = Console.ReadLine().Split();
-            double S = double.Parse(input[0]);
-            int A = int.Parse(input[1]);
-            int T = int.Parse(input[2]);
-            int M = int.Parse(input[3]);
+            int N = int.Parse(Console.ReadLine());
+            double totalScore = 0;
 
-            double score = (S * (1 + (1.0 / A)) * (1 + ((double)M / T))) / 4;
-            totalScore += score;
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                double S = double.Parse(inputs[0]);
+                double A = double.Parse(inputs[1]);
+                double T = double.Parse(inputs[2]);
+                double M = double.Parse(inputs[3]);
+
+                totalScore += CalculScore(S, A, T, M);
+            }
+
+            int P = int.Parse(Console.ReadLine());
+            List<double> scores = new List<double>();
+
+            for (int i = 0; i < P; i++)
+            {
+                double R = double.Parse(Console.ReadLine());
+                scores.Add(R);
+            }
+
+            scores.Add(totalScore);
+            scores = scores.OrderByDescending(x => x).ToList();
+            int rank = scores.IndexOf(totalScore) + 1;
+            double markPoint = (double)rank / scores.Count * 100;
+
+            if (markPoint <= 15)
+            {
+                Console.WriteLine($"The total score of Younghoon \"The God\" is {totalScore:F2}.");
+            }
+            else
+            {
+                Console.WriteLine($"The total score of Younghoon is {totalScore:F2}.");
+            }
         }
 
-        int P = int.Parse(Console.ReadLine());
-        List<double> scores = new List<double>();
-
-        for (int i = 0; i < P; i++)
+        static double CalculScore(double S, double A, double T, double M)
         {
-            double R = double.Parse(Console.ReadLine());
-            scores.Add(R);
-        }
-
-        scores.Add(totalScore);
-        scores = scores.OrderByDescending(x => x).ToList();
-        int rank = scores.IndexOf(totalScore) + 1;
-        double markPoint = (double)rank / scores.Count * 100;
-
-        if (markPoint <= 15)
-        {
-            Console.WriteLine($"The total score of Younghoon \"The God\" is {totalScore:F2}.");
-        }
-        else
-        {
-            Console.WriteLine($"The total score of Younghoon is {totalScore:F2}.");
+            return S * (1 + (1 / A)) * (1 + (M / T)) / 4;
         }
     }
 }
