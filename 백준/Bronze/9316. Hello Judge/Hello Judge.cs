ca66647d2816cd83ad6223514355dcf1e3856a4e
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-10-27 02:39:26 +0900

    [Bronze IV] Title: Hello Judge, Time: 56 ms, Memory: 5748 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cs
@@ -0,0 +1,15 @@
+namespace _9316
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Hello World, Judge {i}!");
+            }
+        }
+    }
+}
