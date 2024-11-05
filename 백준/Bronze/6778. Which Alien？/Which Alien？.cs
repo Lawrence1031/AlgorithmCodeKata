cadbec9193ce1e4a1a00cf4529cd089a91bf344d
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-11-05 13:53:18 +0900

    [Bronze IV] Title: Which Alien?, Time: 48 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.cs
@@ -0,0 +1,26 @@
+namespace _6778
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int antenna = int.Parse(Console.ReadLine());
+            int eyes = int.Parse(Console.ReadLine());
+
+            if (antenna >= 3 && eyes <= 4)
+            {
+                Console.WriteLine("TroyMartian");
+            }
+
+            if (antenna <= 6 && eyes >= 2)
+            {
+                Console.WriteLine("VladSaturnian");
+            }
+
+            if (antenna <= 2 && eyes <= 3)
+            {
+                Console.WriteLine("GraemeMercurian");
+            }
+        }
+    }
+}
