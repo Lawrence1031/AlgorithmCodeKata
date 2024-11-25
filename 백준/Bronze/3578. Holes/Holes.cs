754b0b34838c04c31474b447bed37e3bbbf7c6f9
조성민 <144416099+Lawrence1031@users.noreply.github.com>
2024-11-25 12:03:23 +0900

    [Bronze III] Title: Holes, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

--- 백준/Bronze/3578. Holes/Holes.cs
+++ 백준/Bronze/3578. Holes/Holes.cs
@@ -6,31 +6,21 @@ namespace _3578
         {
             int n = int.Parse(Console.ReadLine());
 
-            int eightCount = n / 2;
-            int left = n % 2;
-
-            if (eightCount == 0)
+            if (n == 0)
             {
-                if (n == 0)
-                {
-                    Console.WriteLine(1);
-                }
-                else
-                {
-                    Console.WriteLine(0);
-                }
+                Console.WriteLine(1);
+            }
+            else if (n == 1)
+            {
+                Console.WriteLine(0);
             }
             else
             {
-                if (left == 0)
-                {
-                    Console.WriteLine(new string('8', eightCount));
-                }
-                else
-                {
-                    Console.Write(4);
-                    Console.WriteLine(new string('8', eightCount));
-                }
+                int eightCount = n / 2;
+                int left = n % 2;
+
+                Console.Write(left == 1 ? "4" : "");
+                Console.WriteLine(new string('8', eightCount));
             }
         }
     }
